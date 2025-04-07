1390f8460d5f0bfb34c6190563654db00794408e
xbze3 <ezranminty@gmail.com>
2025-04-07 15:37:44 -0400

    Fixed routing in navbar

--- physilearn-frontend/src/components/NavBar.tsx
+++ physilearn-frontend/src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import PL_LOGO from "../assets/pl-logo.png";
+import { Link } from "react-router-dom";
 import "../components-css/NavBar.css";
 
 function NavBar() {
@@ -9,13 +10,17 @@ function NavBar() {
         <>
             <Navbar bg="primary" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home" id="navbar-brand">
+                    <Navbar.Brand as={Link} to={`/dashboard`} id="navbar-brand">
                         <img src={PL_LOGO} alt="PL LOGO" id="pl-logo" />
                         PhysiLearn
                     </Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Dashboard</Nav.Link>
-                        <Nav.Link href="#features">My Courses</Nav.Link>
+                        <Nav.Link as={Link} to={`/dashboard`}>
+                            Dashboard
+                        </Nav.Link>
+                        <Nav.Link as={Link} to={`/courses`}>
+                            My Courses
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
