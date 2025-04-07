7cbfb05f0cbb8f2e51ad3fe05597887e72f865e8
xbze3 <ezranminty@gmail.com>
2025-04-07 18:55:03 -0400

    Made assignment list scrollable

--- physilearn-frontend/src/components/AssignmentList.tsx
+++ physilearn-frontend/src/components/AssignmentList.tsx
@@ -7,6 +7,150 @@ function AssignmentList() {
         <ListGroup as="ol" numbered id="assignment-list-group">
             <ListGroup.Item
                 as="li"
+                className="d-flex justify-content-between align-items-start assignment-item"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
                 className="d-flex justify-content-between align-items-start"
             >
                 <div className="ms-2 me-auto">
