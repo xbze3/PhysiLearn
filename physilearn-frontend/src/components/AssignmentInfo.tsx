84fa7478f6d01ba97ceb6072997d47c3f6f0fd89
xbze3 <ezranminty@gmail.com>
2025-04-07 19:27:07 -0400

    Update assignment info styling

--- physilearn-frontend/src/components/AssignmentInfo.tsx
+++ physilearn-frontend/src/components/AssignmentInfo.tsx
@@ -2,9 +2,20 @@ import "../components-css/AssignmentInfo.css";
 
 function AssignmentInfo() {
     return (
-        <div id="assignment-info-div">
-            <h1>Assignment Detail go here</h1>
-            <h1>EZRA DON'T FORGET</h1>
+        <div id="assignment-info-box">
+            <div id="assignment-info-div">
+                <h1>CSE 2201 - Internet Computing I</h1>
+                <h1>Assignment #1</h1>
+
+                <p>Open: April 10th</p>
+                <p>Due: April 14th</p>
+
+                <h1>Submission Status</h1>
+
+                <p>Submission Status: No submissions have been made yet</p>
+                <p>Grading Status: Not yet graded</p>
+                <p>Time remaining: 4 days</p>
+            </div>
         </div>
     );
 }
