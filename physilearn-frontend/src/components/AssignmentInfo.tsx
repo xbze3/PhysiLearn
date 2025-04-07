2a8f2dc1dbeaa3fbc64cd3df66487a1f5b691df0
xbze3 <ezranminty@gmail.com>
2025-04-07 19:06:12 -0400

    Add assignment list item structure

--- physilearn-frontend/src/components/AssignmentInfo.tsx
+++ physilearn-frontend/src/components/AssignmentInfo.tsx
@@ -3,7 +3,8 @@ import "../components-css/AssignmentInfo.css";
 function AssignmentInfo() {
     return (
         <div id="assignment-info-div">
-            <h1>Assignment Info Section</h1>
+            <h1>Assignment Detail go here</h1>
+            <h1>EZRA DON'T FORGET</h1>
         </div>
     );
 }
