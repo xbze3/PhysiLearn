74c3072db1a0cbc37534266e125d4017366d474a
xbze3 <ezranminty@gmail.com>
2025-04-07 17:23:38 -0400

    Made navbar responsive

--- physilearn-frontend/src/components/NavBar.tsx
+++ physilearn-frontend/src/components/NavBar.tsx
@@ -1,30 +1,48 @@
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import PL_LOGO from "../assets/pl-logo.png";
-import { Link } from "react-router-dom";
 import "../components-css/NavBar.css";
 
 function NavBar() {
     return (
-        <>
-            <Navbar bg="primary" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand as={Link} to={`/dashboard`} id="navbar-brand">
-                        <img src={PL_LOGO} alt="PL LOGO" id="pl-logo" />
-                        PhysiLearn
-                    </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to={`/dashboard`}>
+        <Navbar expand="lg" bg="primary" data-bs-theme="dark">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/dashboard" id="navbar-brand">
+                    <img src={PL_LOGO} alt="PL LOGO" id="pl-logo" />
+                    PhysiLearn
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: "100px" }}
+                        navbarScroll
+                    >
+                        <Nav.Link as={Link} to="/dashboard">
                             Dashboard
                         </Nav.Link>
-                        <Nav.Link as={Link} to={`/courses`}>
+                        <Nav.Link as={Link} to="/courses">
                             My Courses
                         </Nav.Link>
+                        <NavDropdown title="More" id="navbarScrollingDropdown">
+                            <NavDropdown.Item as={Link} to="/profile">
+                                Profile
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/settings">
+                                Settings
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="/logout">
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
-                </Container>
-            </Navbar>
-        </>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
